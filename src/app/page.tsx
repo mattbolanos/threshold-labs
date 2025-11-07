63e0369e8e92f168cea8f7f00c6a0e84d781754b
mattbolanos <matthew.a.bolanos@gmail.com>
2025-11-07 17:42:11 -0500

    let's do

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { CalendarBlocks } from "@/components/calendar/calendar-blocks";
 import { CalendarHeaderText } from "@/components/calendar/calendar-header-text";
 import { DayBlocks } from "@/components/calendar/day-blocks";
 import { api } from "@/trpc/server";
+import { Separator } from "../components/ui/separator";
 
 export default function Home() {
   return (
@@ -13,7 +14,7 @@ export default function Home() {
         className="bg-background sticky z-20 -mx-5 sm:-mx-6"
         style={{ top: "max(0px, env(safe-area-inset-top))" }}
       >
-        <div className="flex flex-col justify-between gap-6 px-5 py-3 sm:px-6 sm:py-4 md:flex-row md:items-center">
+        <div className="flex flex-col justify-between gap-3 px-5 py-3 sm:px-6 sm:py-4 md:flex-row md:items-center">
           <Suspense fallback={<div>Loading...</div>}>
             <CalendarHeaderText />
             <CalendarArrows
@@ -21,9 +22,10 @@ export default function Home() {
             />
           </Suspense>
         </div>
+        <Separator className="data-[orientation=horizontal]:h-0.5 md:hidden" />
       </div>
 
-      <div className="flex flex-col gap-2">
+      <div className="mt-4 flex flex-col gap-2 md:mt-0">
         <Suspense fallback={<div>Loading...</div>}>
           <DayBlocks />
         </Suspense>

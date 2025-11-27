5e7fcc8c7c589cbd0059af95b3512a02607d068d
mattbolanos <matthew.a.bolanos@gmail.com>
2025-11-27 11:45:36 -0800

    added posthog

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   cacheComponents: true,
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+        source: "/ingest/static/:path*",
+      },
+      {
+        destination: "https://us.i.posthog.com/:path*",
+        source: "/ingest/:path*",
+      },
+    ];
+  },
+  skipTrailingSlashRedirect: true,
   typedRoutes: true,
 };
 

f6e3f3a117f09c6ad0079273db689ab5426f046b
Matt Bolaños <69167336+mattbolanos@users.noreply.github.com>
2025-11-27 15:02:42 -0800

    Update day-blocks.tsx

--- src/components/skeletons/day-blocks.tsx
+++ src/components/skeletons/day-blocks.tsx
@@ -1,15 +1,12 @@
 import { Skeleton } from "@/components/ui/skeleton";
-import { getWeekDays } from "@/lib/utils";
 
 export function DayBlocksSkeleton() {
-  const weekDays = getWeekDays(new Date());
-
   return (
     <div className="hidden grid-cols-7 gap-2 md:grid">
-      {weekDays.map((day) => (
+      {Array.from({ length: 7 }).map((_, i) => (
         <div
           className="bg-card col-span-1 mx-auto flex w-full flex-col items-center justify-center gap-1 rounded-md border py-1 text-center font-medium"
-          key={day.toISOString()}
+          key={i}
         >
           <Skeleton className="h-3 w-10" />
           <Skeleton className="mx-auto size-7 rounded-full" />

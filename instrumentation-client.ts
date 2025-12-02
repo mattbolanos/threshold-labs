f5bdb0f896a95ca0067030061f1698db6725b8cf
mattbolanos <matthew.a.bolanos@gmail.com>
2025-12-01 21:56:56 -0500

    stash

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -4,6 +4,6 @@ posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
   api_host: "/ingest",
   capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
   debug: process.env.NODE_ENV === "development",
-  defaults: "2025-05-24",
+  defaults: "2025-11-30",
   ui_host: "https://us.posthog.com",
 });

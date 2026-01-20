4e00879f6552f9823c9b9b9dea9b760952fc74cd
mattbolanos <matthew.a.bolanos@gmail.com>
2026-01-20 17:20:02 -0500

    rm posthot + add convex

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,8 +1,13 @@
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 import { ThemeProvider } from "@/components/theme/theme-provider";
 import { TRPCReactProvider } from "@/trpc/react";
 import { HydrateClient } from "@/trpc/server";
+
+const convex = new ConvexReactClient(
+  process.env.NEXT_PUBLIC_CONVEX_URL as string,
+);
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
@@ -10,14 +15,16 @@ export function Providers({ children }: { children: React.ReactNode }) {
       <TRPCReactProvider>
         <ReactQueryDevtools />
         <HydrateClient>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            disableTransitionOnChange
-            enableSystem
-          >
-            {children}
-          </ThemeProvider>
+          <ConvexProvider client={convex}>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              disableTransitionOnChange
+              enableSystem
+            >
+              {children}
+            </ThemeProvider>
+          </ConvexProvider>
         </HydrateClient>
       </TRPCReactProvider>
     </NuqsAdapter>

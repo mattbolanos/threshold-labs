c223bc94ab380e4ae03b891476f68dd4dbfed3d0
mattbolanos <matthew.a.bolanos@gmail.com>
2026-08-22 21:24:52 -0700

    rm unused

--- src/components/workout-library/workout-library-results.tsx
+++ src/components/workout-library/workout-library-results.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { IconCalendarStats, IconSearch } from "@tabler/icons-react";
+import { IconSearch } from "@tabler/icons-react";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
